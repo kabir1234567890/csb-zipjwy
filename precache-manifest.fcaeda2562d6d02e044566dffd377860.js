dee1f780134b52c9bf2efb8b025d40b6b77a30d8
Code Sandbox <hello@codesandbox.io>
2023-03-03 06:27:14 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.fcaeda2562d6d02e044566dffd377860.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "981dddb972f70d648c62",
+    "url": "https://.github.io/csb-zipjwy/static/js/main.981dddb9.chunk.js"
+  },
+  {
+    "revision": "fb928938ad273cdfb674",
+    "url": "https://.github.io/csb-zipjwy/static/css/1.ae982edb.chunk.css"
+  },
+  {
+    "revision": "fb928938ad273cdfb674",
+    "url": "https://.github.io/csb-zipjwy/static/js/1.fb928938.chunk.js"
+  },
+  {
+    "revision": "79f566b8806246356be5",
+    "url": "https://.github.io/csb-zipjwy/static/js/runtime~main.79f566b8.js"
+  },
+  {
+    "revision": "7e263200d01fb5998c401b03ef3e6433",
+    "url": "https://.github.io/csb-zipjwy/static/media/summernote.7e263200.eot"
+  },
+  {
+    "revision": "549304631a5f1ab69498e294543086e3",
+    "url": "https://.github.io/csb-zipjwy/static/media/summernote.54930463.ttf"
+  },
+  {
+    "revision": "03f45d882ff3a80b719cfc0789c5eab9",
+    "url": "https://.github.io/csb-zipjwy/static/media/summernote.03f45d88.woff"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "https://.github.io/csb-zipjwy/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "https://.github.io/csb-zipjwy/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "https://.github.io/csb-zipjwy/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "https://.github.io/csb-zipjwy/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "https://.github.io/csb-zipjwy/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e9026b80e290db2bfd28676056bd70ea",
+    "url": "https://.github.io/csb-zipjwy/index.html"
+  }
+];
